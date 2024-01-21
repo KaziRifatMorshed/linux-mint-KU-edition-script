c22e955422a1e1b1a09ed837833de64ac463f329
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-01-22 00:01:18 +0600

    Update and rename linux-mint-ku-edition to linux-mint-ku-edition.sh

---
+++ linux-mint-ku-edition.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Welcome, This is an automated script to turn a typical(normal) Linux Mint installation into a user-friendly one with KU themes
+echo We want to provide a malware-free fast computing experience to the next generation and revive old machines
+echo "for further information, visit XYZ"
+
+
+
+<!--KaziRifatMorshed|KU|CSE23|230220-->
