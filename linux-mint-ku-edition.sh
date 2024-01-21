5a19781aa155136d9e883097cef1d42c8151df18
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-01-22 00:14:27 +0600

    Update linux-mint-ku-edition.sh

--- linux-mint-ku-edition.sh
+++ linux-mint-ku-edition.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo Welcome, This is an automated script to turn a typical(normal) Linux Mint installation into a user-friendly one with KU themes
-echo We want to provide a malware-free fast computing experience to the next generation and revive old machines
+echo We want to provide a malware-free out-of-the-box computing experience to the next generation and revive old machines
 echo "for further information, visit XYZ"
 
 
-
+ # when done
+ echo Completed !!! Enjoy Linux Mint !!! LEARN, LIVE and LEAD
 <!--KaziRifatMorshed|KU|CSE23|230220-->
