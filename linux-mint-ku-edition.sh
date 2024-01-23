12c9ca0663de2bed320fa8ae3f9802a36152469a
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-01-24 00:10:12 +0600

    Update linux-mint-ku-edition.sh

--- linux-mint-ku-edition.sh
+++ linux-mint-ku-edition.sh
@@ -4,6 +4,13 @@ echo We want to provide a malware-free out-of-the-box computing experience to th
 echo "for further information, visit XYZ"
 
 
+## Get root password for proceeding
+echo Plz write your root password (CORRECTLY, in case it failed, start from the beginning) so that you can relax and we can handle all stuffs...
+echo ROOT PASS: 
+
+## User Preferation
+#echo Do you want to make the installation process totally automatic or will you make choices ???
+
  # when done
- echo Completed !!! Enjoy Linux Mint !!! LEARN, LIVE and LEAD
+ echo Completed !!! Enjoy Linux Mint !!! LEARN, LIVE and LEAD :) 
 <!--KaziRifatMorshed|KU|CSE23|230220-->
